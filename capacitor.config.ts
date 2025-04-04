86a3c625c2f4f30b5c0676c9e0dc605df10999d2
rbyers87 <35844687+rbyers87@users.noreply.github.com>
2025-04-04 14:15:09 -0500

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ryby.workouttracker',
+  appName: 'workout-tracker',
+  webDir: 'dist'
+};
+
+export default config;
